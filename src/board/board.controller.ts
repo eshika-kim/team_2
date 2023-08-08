7009102b9f2064c656f727ebf64859e5d705ba8b
Taehwan Oh <othwan410@daum.net>
2023-08-08 16:48:56 +0900

    temp

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BoardService } from './board.service';
+import { CreateBoardDto } from '../dto/board/create-board.dto';
+import { UpdateBoardDto } from '../dto/board/update-board.dto';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
+  @Get('/board')
+  async getBoards() {
+    return await this.boardService.getBoards();
+  }
+
+  @Post('/board')
+  createBoard(@Body() data: CreateBoardDto) {
+    return this.boardService.createBoard(
+      data.name,
+      data.color,
+      data.description,
+    );
+  }
+
+  @Put('/board/:board_id')
+  async updateBoard(
+    @Param('board_id') board_id: number,
+    @Body() data: UpdateBoardDto,
+  ) {
+    return await this.boardService.updateBoard(board_id, data.name);
+  }
+
+  @Delete('/board/:board_id')
+  async deleteArticle(@Param('board_id') articleId: number) {
+    return await this.boardService.deleteArticle(board_id);
+  }
+}
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Board } from '../entity/board.entity';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  constructor(
+    @InjectRepository(Board) private articleRepository: Repository<Board>,
+  ) {}
+
+  async getBoards(user_id: number) {
+    return await this.articleRepository.find({
+      where: { deletedAt: null, user_id },
+      select: [
+        'board_id',
+        'waiting',
+        'user_id',
+        'name',
+        'color',
+        'description',
+      ],
+    });
+  }
+
+  async getArticleById(id: number) {
+    return await this.articleRepository.findOne({
+      where: { id, deletedAt: null },
+      select: ['author', 'title', 'content', 'createdAt', 'updatedAt'],
+    });
+  }
+
+  createArticle(title: string, content: string, password: number) {
+    this.articleRepository.insert({
+      // 일단, 편의를 위해 author는 잠시 test로 고정합니다.
+      author: 'test',
+      title,
+      content,
+      // 마찬가지로, password도 잠시 숫자를 문자열로 바꾸겠습니다.
+      // 나중에 암호화된 비밀번호를 저장하도록 하겠습니다.
+      password: password.toString(),
+    });
+  }
+
+  async updateArticle(
+    id: number,
+    title: string,
+    content: string,
+    password: number,
+  ) {
+    await this.checkPassword(id, password);
+    this.articleRepository.update(id, { title, content });
+  }
+
+  async deleteArticle(id: number, password: number) {
+    await this.checkPassword(id, password);
+    this.articleRepository.softDelete(id); // soft delete를 시켜주는 것이 핵심!
+  }
+
+  private async checkPassword(id: number, password: number) {
+    const article = await this.articleRepository.findOne({
+      where: { id, deletedAt: null },
+      select: ['password'],
+    });
+    if (_.isNil(article)) {
+      throw new NotFoundException(`Article not found. id: ${id}`);
+    }
+
+    if (article.password !== password.toString()) {
+      throw new UnauthorizedException(
+        `Article password is not correct. id: ${id}`,
+      );
+    }
+  }
+}
