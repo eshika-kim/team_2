f603d63c515eef99875710d403a686ce39d9fd63
baechanyong1 <110149885+baechanyong1@users.noreply.github.com>
2023-08-09 17:16:02 +0900

    카드 두 번째 커밋 카드 상세 조회 기능 구현 완

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { CardController } from './card.controller';
 import { CardService } from './card.service';
 import {Card} from '../entity/card.entity'
-
+import { Comment } from '../entity/comment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card])],
+  imports: [TypeOrmModule.forFeature([Card,Comment])],
   controllers: [CardController],
   providers: [CardService]
 })
